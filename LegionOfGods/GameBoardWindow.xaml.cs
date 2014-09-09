2ba0b43ae2a47e921a4c5a29e3e29dd3d4f3d77c
Vladimir Mikryukov <motobiker2008@gmail.com>
2014-09-09 10:54:23 +0400

    Pre implemented GameBoard

--- LegionOfGods/GameBoardWindow.xaml.cs
+++ LegionOfGods/GameBoardWindow.xaml.cs
@@ -9,25 +9,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace LegionOfGods
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GameBoardWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GameBoardWindow : Window
     {
-        public MainWindow()
+        public GameBoardWindow()
         {
             InitializeComponent();
-        }
-
-        private void OnGameClicked(object sender, MouseButtonEventArgs e)
-        {
-            GameBoardWindow gbw = new GameBoardWindow();
-            gbw.Show();
         }
     }
 }
--- LegionOfGods/obj/x86/Debug/LegionOfGods_Content.g.i.cs
+++ LegionOfGods/obj/x86/Debug/LegionOfGods_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/button_settings.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/backgroun.jpg")]
 
 
